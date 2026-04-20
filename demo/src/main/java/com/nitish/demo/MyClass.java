e1d6be4d292fd5325cd0b6f67795967abdaa6c7f
Nitish Kumar <nk045729@gmail.com>
2026-04-20 17:27:20 +0530

    update

--- demo/src/main/java/com/nitish/demo/MyClass.java
+++ demo/src/main/java/com/nitish/demo/MyClass.java
@@ -3,7 +3,7 @@ package com.nitish.demo;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController   
 public class MyClass {
 
     @GetMapping("/123")
